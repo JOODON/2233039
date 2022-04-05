3b3f7e4269dbad15dc2bc7921889f30d75529221
주동호 <launcher37@naver.com>
2022-04-05 14:57:26 +0900

    1

---
+++ main.js
@@ -0,0 +1,18 @@
+window.onload=function(){
+b1.onclick=function(){
+name=prompt("이름을 입력하세요");
+ this.document.getElementsByClassName("name")[0].innerHTML="Name:"+name;
+};
+};
+window.onload=function(){
+b3.onclick=function(){
+number=prompt("학번를 입력하세요");
+this.document.getElementsByClassName("Number")[0].innerHTML="Number:"+age;
+};
+};
+window.onload=function(){
+b3.onclick=function(){
+age=prompt("나이를 입력하세요");
+this.document.getElementsByClassName("age")[0].innerHTML="age:"+age;
+};
+};
